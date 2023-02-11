4cf9ff49b90ad1b09226cecafef0351a1e8dd361
prettyturtle <unifconti@gmail.com>
2023-02-11 23:15:59 +0900

    TabIcon jsx -> tsx

---
+++ src/components/TabIcon.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { Badge } from "./Badge"
+import { Icon, IconName } from "./Icon"
+
+export const TabIcon: React.FC<{
+  visibleBadge: boolean
+  iconName: IconName
+  iconColor: string
+}> = (props) => {
+  if (props.visibleBadge) {
+    return (
+      <Badge>
+        <Icon
+          name={props.iconName}
+          size={20}
+          color={props.iconColor}
+        />
+      </Badge>
+    )
+  }
+
+  return (
+    <Icon
+      name={props.iconName}
+      size={20}
+      color={props.iconColor}
+    />
+  )
+}
