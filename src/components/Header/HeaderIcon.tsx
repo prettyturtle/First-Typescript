0e04da6bbb565c37d237dd3337e1c8cdf562ff39
prettyturtle <unifconti@gmail.com>
2023-02-12 20:26:58 +0900

    Header jsx -> tsx

---
+++ src/components/Header/HeaderIcon.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { Button } from "../Button"
+import { Icon, IconName } from "../Icon"
+
+export const HeaderIcon: React.FC<{
+  onPress: () => void
+  iconName: IconName
+}> = (props) => {
+  return (
+    <Button onPress={props.onPress}>
+      <Icon
+        name={props.iconName}
+        size={28}
+        color="black"
+      />
+    </Button>
+  )
+}
